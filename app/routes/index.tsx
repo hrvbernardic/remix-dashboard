117839d05f4f746658ffb6f3b1f99d600e7d9c42
Hrvoje Bernardić <hrv.bernardic@gmail.com>
2021-12-22 21:52:00 +0100

    add sqlserver db and prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,18 @@
+import { PrismaClient, User } from "@prisma/client";
+import { LoaderFunction, useLoaderData } from "remix";
+
+export const loader: LoaderFunction = async () => {
+  const prisma = new PrismaClient();
+
+  return prisma.user.findFirst();
+}
+
 export default function Index() {
+  const user = useLoaderData<User>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Welcome to Remix, {user.email}</h1>
     </div>
   );
 }
