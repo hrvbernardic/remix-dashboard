cb04bb085544568467cdbcaa9206399f56cd116e
Hrvoje Bernardić <hrv.bernardic@gmail.com>
2021-12-24 18:42:23 +0100

    add prettier as eslint plugin

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,18 @@
-import { PrismaClient, User } from "@prisma/client";
-import { LoaderFunction, useLoaderData } from "remix";
+import { PrismaClient, User } from '@prisma/client';
+import { LoaderFunction, useLoaderData } from 'remix';
 
 export const loader: LoaderFunction = async () => {
-  const prisma = new PrismaClient();
+    const prisma = new PrismaClient();
 
-  return prisma.user.findFirst();
-}
+    return prisma.user.findFirst();
+};
 
 export default function Index() {
-  const user = useLoaderData<User>();
+    const user = useLoaderData<User>();
 
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix, {user.email}</h1>
-    </div>
-  );
+    return (
+        <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+            <h1>Welcome to Remix, {user.email}</h1>
+        </div>
+    );
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,15 +3,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function seed() {
-  await prisma.user.create({
-    data: {
-      email: 'hrvoje.bernardic@gmail.com'
-    }
-  });
+    await prisma.user.create({
+        data: {
+            email: 'hrvoje.bernardic@gmail.com',
+        },
+    });
 }
 
 seed();
 
-
 // needed to be considered a module
-export { };
+export {};
